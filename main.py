d91e26766ea9c8b52431927ec796bc4a13168de7
Daliana-Solis <dsolisso@uci.edu>
2023-09-24 18:04:00 -0500

    Set Receipt class

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import pandas as pd
 
+#Make ID column a string
+df = pd.read_csv("hotels.csv", dtype={"id":str})
 
-df = pd.read_csv("hotels.csv")
+
 class Hotel:
-
-    def __int__(self, hotel_id):
-        self.hotel_id = hotel_id
+    def __init__(self, hotel_ID):
+        self.hotel_id = hotel_ID
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
 
     def available(self):
         """Checks if hotel is available """
-
         availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
 
         if availability == "yes":
             return True
         else:
             return False
-
-
 
     def book(self):
         """Books hotel by changing availability status to no """
@@ -29,28 +28,36 @@ class Hotel:
         pass
 
 
-class ReservationConfirmation:
 
+class ReservationConfirmation:
     def __init__(self, customer_name,  hotel):
         self.customer_name = customer_name
         self.hotel = hotel
+
     def generate(self):
-        content = f"Name of the customer hotel"
+        content = f"""
+        Thank you for your reservation! 
+        Here is your booking data:
+        
+        Name: {self.customer_name}
+        Hotel Name: {self.hotel.name}
+        """
         return content
 
 
-print(df)
 
-hotel_id = input("Enter the id of the hotel: ")
-hotel = Hotel(hotel_id)
+
+print(df)
+hotel_ID = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_ID)
 
 #If hotel available
 if hotel.available():
     hotel.book()
-    name = input("Enter your name: ")
+    customer_name = input("Enter your name: ")
 
     #Call Reservation class  and get confirmation
-    reservation_confirm = ReservationConfirmation(name, hotel)
+    reservation_confirm = ReservationConfirmation(customer_name, hotel)
     print(reservation_confirm.generate())
 
 else:
