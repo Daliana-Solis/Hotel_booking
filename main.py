869efd0cf20f277b815e5d779640e3066a0d9fa7
Daliana-Solis <dsolisso@uci.edu>
2023-09-24 18:23:45 -0500

    Added simple credit card feature

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 #Make ID column a string
 df = pd.read_csv("hotels.csv", dtype={"id":str})
 
+df_cards = pd.read_csv("cards.csv", dtype=str).to_dict(orient="records")
+
 
 class Hotel:
     def __init__(self, hotel_ID):
@@ -24,10 +26,6 @@ class Hotel:
         #updates the CSV file
         df.to_csv("hotels.csv", index=False)
 
-    def view_hotels(self):
-        pass
-
-
 
 class ReservationConfirmation:
     def __init__(self, customer_name,  hotel):
@@ -44,21 +42,42 @@ class ReservationConfirmation:
         """
         return content
 
+class CreditCard:
+
+    def __init__(self, card_num):
+        self.num = card_num
+
+    def validate(self, exp_date, cvs, name):
+        card_info = {"number": self.num, "expiration": exp_date,
+                     "holder": name, "cvc": cvs}
+
+        if card_info in df_cards:
+            return True
+        return False
 
 
 
 print(df)
+
+#Get ID user wants to book
 hotel_ID = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_ID)
 
-#If hotel available
+#If hotel available, book
 if hotel.available():
-    hotel.book()
-    customer_name = input("Enter your name: ")
 
-    #Call Reservation class  and get confirmation
-    reservation_confirm = ReservationConfirmation(customer_name, hotel)
-    print(reservation_confirm.generate())
+    credit_card = CreditCard('1234567890123456')
+
+    #If valid credit card, then process with booking
+    if credit_card.validate('12/26', '123', 'JOHN SMITH'):
+        hotel.book()
+        customer_name = input("Enter your name: ")
+
+        #Call Reservation class  and get confirmation
+        reservation_confirm = ReservationConfirmation(customer_name, hotel)
+        print(reservation_confirm.generate())
+    else:
+        print("There was a problem with your payment.")
 
 else:
     print("Hotel is not available")
